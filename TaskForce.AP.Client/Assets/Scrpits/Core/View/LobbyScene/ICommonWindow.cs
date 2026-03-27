bef7af24d6dc78feb93bb34371ff63e57f7f5a99
freeoss <freeoss@naver.com>
2026-03-27 19:46:45 +0900

    IWindow 폴더 변경 적용

--- TaskForce.AP.Client/Assets/Scrpits/Core/View/LobbyScene/ICommonWindow.cs
+++ TaskForce.AP.Client/Assets/Scrpits/Core/View/LobbyScene/ICommonWindow.cs
@@ -1,5 +1,5 @@
 using System;
-using TaskForce.AP.Client.Core.View.BattleFieldScene.Windows;
+using TaskForce.AP.Client.Core.View.Windows;
 
 namespace TaskForce.AP.Client.Core.View.LobbyScene.Windows
 {
--- TaskForce.AP.Client/Assets/Scrpits/Core/View/LobbyScene/IEnergyGetWindow.cs
+++ TaskForce.AP.Client/Assets/Scrpits/Core/View/LobbyScene/IEnergyGetWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using TaskForce.AP.Client.Core.View.BattleFieldScene.Windows;
+using TaskForce.AP.Client.Core.View.Windows;
 
 namespace TaskForce.AP.Client.Core.View.LobbyScene.Windows
 {

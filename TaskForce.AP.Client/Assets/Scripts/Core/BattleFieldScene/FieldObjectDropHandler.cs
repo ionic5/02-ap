1c3a3f0eefb7ee85a55740a152eeb7c7da0f86d7
ionic5 <st.kaldalis@gmail.com>
2026-04-04 15:07:07 +0900

    feat: RootBox 파괴 시 MedicalKit 드롭 구현
    - MedicalKit: IFieldItem 구현체 추가
    - FieldItemFactory: MedicalKit 생성 팩토리 추가
    - FieldObjectDropHandler: FieldItemFactory 주입, OnRootBoxDied 메서드 추가

--- TaskForce.AP.Client/Assets/Scripts/Core/BattleFieldScene/FieldObjectDropHandler.cs
+++ TaskForce.AP.Client/Assets/Scripts/Core/BattleFieldScene/FieldObjectDropHandler.cs
@@ -5,12 +5,15 @@ namespace TaskForce.AP.Client.Core.BattleFieldScene
     public class FieldObjectDropHandler
     {
         private readonly ExpOrbFactory _expOrbFactory;
+        private readonly FieldItemFactory _fieldItemFactory;
         private readonly Core.Random _random;
         private readonly GameDataStore _gameDataStore;
 
-        public FieldObjectDropHandler(ExpOrbFactory expOrbFactory, Core.Random random, GameDataStore gameDataStore)
+        public FieldObjectDropHandler(ExpOrbFactory expOrbFactory, FieldItemFactory fieldItemFactory,
+            Core.Random random, GameDataStore gameDataStore)
         {
             _expOrbFactory = expOrbFactory;
+            _fieldItemFactory = fieldItemFactory;
             _random = random;
             _gameDataStore = gameDataStore;
         }
@@ -32,6 +35,16 @@ namespace TaskForce.AP.Client.Core.BattleFieldScene
         {
             var expOrb = _expOrbFactory.Create(1);
             expOrb.SetPosition(args.DiedTarget.GetPosition());
+        }
+
+        public void OnRootBoxDied(object sender, DiedEventArgs args)
+        {
+            var fieldItemData = _gameDataStore.GetFieldItem(GameData.FieldItemID.MedicalKit);
+            if (fieldItemData == null)
+                return;
+
+            var item = _fieldItemFactory.CreateMedicalKit();
+            item.SetPosition(args.DiedTarget.GetPosition());
         }
     }
 }

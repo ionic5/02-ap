4906bdd26049ec1f7f58abd0800f32e0fd8cc970
leomanic <croasoap@gmail.com>
2026-04-08 09:27:41 +0900

    Pistol 속성 변경
    min/max 제외, 단일 속성으로 변경

--- TaskForce.AP.Client/Assets/Scripts/UnityWorld/BattleFieldSceneLoader.cs
+++ TaskForce.AP.Client/Assets/Scripts/UnityWorld/BattleFieldSceneLoader.cs
@@ -160,10 +160,10 @@ namespace TaskForce.AP.Client.UnityWorld
             skillFactory.AddCreator(Core.Entity.SkillID.PistolAttack, (skill) => // SkillID.Pistol -> SkillID.PistolAttack
             {
                 // Core.BattleFieldScene.Skills.Bullet을 생성하는 람다 함수
-                return new PistolSkill((caster, minDmg, maxDmg, finder) =>
+                return new PistolSkill((caster, dmg, finder) =>
                 {
                     var view = objFac.Create<View.BattleFieldScene.Bullet>(ObjectID.Bullet); // Unity View Bullet 생성
-                    var bullet = new Core.BattleFieldScene.Skills.Bullet(_random, view, finder, caster, minDmg, maxDmg);
+                    var bullet = new Core.BattleFieldScene.Skills.Bullet(_random, view, finder, caster, dmg);
                     
                     loop.Add(bullet); // Bullet을 루프에 추가하여 Update가 호출되도록 함
                     

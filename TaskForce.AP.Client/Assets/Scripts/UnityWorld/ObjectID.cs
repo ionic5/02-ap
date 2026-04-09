b2cedf95ad472b5126f7ea6dd594f86dd628a5be
leomanic <croasoap@gmail.com>
2026-04-09 22:48:20 +0900

    수류탄 파티클 이펙트 및 소스 추가
    수류탄 폭발 효과 적용

--- TaskForce.AP.Client/Assets/Scripts/UnityWorld/ObjectID.cs
+++ TaskForce.AP.Client/Assets/Scripts/UnityWorld/ObjectID.cs
@@ -1,4 +1,4 @@
-﻿namespace TaskForce.AP.Client.UnityWorld
+namespace TaskForce.AP.Client.UnityWorld
 {
     public class ObjectID
     {
@@ -17,5 +17,6 @@
         public const string FloatingTextAnimator = "FLOATING_TEXT_ANIMATOR";
         public const string Bullet = "BULLET"; // 권총 총알 오브젝트 ID 추가
         public const string SniperBullet = "SNIPER_BULLET";
+        public const string GrenadeExplosion = "GRENADE_EXPLOSION";
     }
 }

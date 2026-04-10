cc9ef34530bc12dbfac2733cbd0485346e029471
leomanic <croasoap@gmail.com>
2026-04-10 10:41:31 +0900

    지뢰, 수류탄 오브젝트 명칭 정정

--- TaskForce.AP.Client/Assets/Scripts/UnityWorld/ObjectID.cs
+++ TaskForce.AP.Client/Assets/Scripts/UnityWorld/ObjectID.cs
@@ -10,13 +10,14 @@ namespace TaskForce.AP.Client.UnityWorld
         public const string Explosion1 = "EXPLOSION_1";
         public const string FieldItem = "FIELD_ITEM";
         public const string RootBox = "ROOT_BOX";
-        public const string Landmine = "POWDER_KEG";
-        public const string Grenade = "DYNAMITE";
+        public const string Landmine = "LANDMINE";
+        public const string Grenade = "GRENADE";
         public const string Rpg = "RPG";
         public const string SheepMissile = "SHEEP_MISSILE";
         public const string FloatingTextAnimator = "FLOATING_TEXT_ANIMATOR";
         public const string Bullet = "BULLET"; // 권총 총알 오브젝트 ID 추가
         public const string SniperBullet = "SNIPER_BULLET";
+        public const string LandmineExplosion = "LANDMINE_EXPLOSION";
         public const string GrenadeExplosion = "GRENADE_EXPLOSION";
     }
 }

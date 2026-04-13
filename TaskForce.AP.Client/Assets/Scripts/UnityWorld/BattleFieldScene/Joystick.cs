2b0d1d88aca8d212d223f798115ea73a90cfcbfa
ionic5 <st.kaldalis@gmail.com>
2026-04-13 11:32:44 +0900

    조이스틱 조작감 불일치 문제 해결

--- TaskForce.AP.Client/Assets/Scripts/UnityWorld/BattleFieldScene/Joystick.cs
+++ TaskForce.AP.Client/Assets/Scripts/UnityWorld/BattleFieldScene/Joystick.cs
@@ -1,23 +1,40 @@
 ﻿using System.Numerics;
 using TaskForce.AP.Client.Core.BattleFieldScene;
+using UnityEngine;
 
 namespace TaskForce.AP.Client.UnityWorld.BattleFieldScene
 {
     public class Joystick : IJoystick
     {
         private readonly global::Joystick _joystick;
-        private Vector2 _inputVector;
+        private readonly Camera _camera;
+        private System.Numerics.Vector2 _inputVector;
 
         public Joystick(global::Joystick joystick)
         {
-            _inputVector = new Vector2();
             _joystick = joystick;
+            _camera = Camera.main;
+            _inputVector = new System.Numerics.Vector2();
         }
 
-        public Vector2 GetInputVector()
+        public System.Numerics.Vector2 GetInputVector()
         {
-            _inputVector.Y = _joystick.Vertical;
-            _inputVector.X = _joystick.Horizontal;
+            float h = _joystick.Horizontal;
+            float v = _joystick.Vertical;
+
+            if (_camera != null)
+            {
+                var camForward = UnityEngine.Vector3.ProjectOnPlane(_camera.transform.forward, UnityEngine.Vector3.up).normalized;
+                var camRight = UnityEngine.Vector3.ProjectOnPlane(_camera.transform.right, UnityEngine.Vector3.up).normalized;
+                var worldDir = camRight * h + camForward * v;
+                _inputVector.X = worldDir.x;
+                _inputVector.Y = worldDir.z;
+            }
+            else
+            {
+                _inputVector.X = h;
+                _inputVector.Y = v;
+            }
 
             return _inputVector;
         }
